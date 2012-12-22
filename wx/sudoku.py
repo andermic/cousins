a5c499c8c916812d552153630a737ead642e65ff
Mayor McCheese <andermic@Mayor-McCheeses-Mac-Pro.local>
2012-12-21 19:45:25 -0800

    just a quick save to move to the office

--- wx/sudoku.py
+++ wx/sudoku.py
@@ -6,6 +6,7 @@ def get_a_board():
 	
 
 def get_sudoku():
+	pass
 
 class BoardWindow:
 	def __init__(s, initial_tiles, parent):
@@ -15,7 +16,7 @@ class BoardWindow:
 
 class MainWindow(wx.Frame):
 	def __init__(s, parent, title):
-		wx.Frame.__init__(s, parent, title=title, size=(200,-1))
+		wx.Frame.__init__(s, parent, title=title, size=(400,-1))
 		initial_tiles = [[choice(range(1,10) + [' ']) for i in range(9)] for j in range(9)]
 
 
@@ -24,7 +25,7 @@ class MainWindow(wx.Frame):
 		# Draw everything
 		#s.board = new Board(initial_tiles)
 		
-		s.tiles = [[wx.Button(s, -1, str(initial_tiles[i][j]), size=(23,23)) for i in range(9)] for j in range(9)]
+		s.tiles = [[wx.Button(s, -1, str(initial_tiles[i][j]), size=(27,25)) for i in range(9)] for j in range(9)]
 		s.grid = wx.GridSizer(9,9)
 		for row in s.tiles:
 			for tile in row:
@@ -33,8 +34,6 @@ class MainWindow(wx.Frame):
 		s.SetSizer(s.grid)
 		s.Show()
 
-"""
 app = wx.App(False)
 MainWindow(None, 'Sudoku Generator')
 app.MainLoop()
-"""
