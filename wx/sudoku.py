5b6f62e5c6ebde4b2e63da18b454f22a0edee164
Mayor McCheese <andermic@Mayor-McCheeses-Mac-Pro.local>
2012-12-22 01:56:49 -0800

    working on recursive backtracking

--- wx/sudoku.py
+++ wx/sudoku.py
@@ -1,9 +1,6 @@
 import wx
 from random import choice
 
-def get_sudoku(difficult):
-	pass
-
 def display(board):
 	for i in range(9):
 		for j in range(3):
@@ -13,7 +10,7 @@ def display(board):
 			print
 
 def check_for_doubles(test_list):
-	no_empty = [i for i in test_list if i != ' ']
+	no_empty = [i for i in test_list if i != '-']
 	if len(set(no_empty)) == len(no_empty):
 		return False
 	return True
@@ -22,46 +19,71 @@ def check_subsquare(board, number):
 	test_list = []
 	for i in range(3):
 		for j in range(3):
-			test_list.append(board[i + number/3, j + number%3])
+			test_list.append(board[i + number/3][j + number%3])
 	return check_for_doubles(test_list)
 
 def check_all(board):
 	for i in range(9):
 		if check_for_doubles(board[i]) or \
                  check_for_doubles([board[j][i] for j in range(9)]) or \
-		 check_subsquare(board(i)):
+		 check_subsquare(board, i):
 			return True
 	return False
 
-def get_a_board():
-	init = [[choose(range(1,10)) for i in range(9)] for j in range(9)]
-	result = [[' ' for i in range(9)] for j in range(9)]
-	i,j = 0,0
-	while i+j <= 18:
-		result[i][j] = init[i][j]
-		if check_all(result):
-			while True:
-				result[i][j] = result[i][j] + 1
-				if result[i][j] == 10:
-					result[i][j] = 1
-				if result[i][j] == init:
-					result[i][j] = ' '
-					j -= 1
-					if j == 0:
-						i -= 1
-						j = 8
-				else:
-					break
-		j += 1
-		if j == 10:
+def gab_recurse(init, cur, i, j):
+	display(init)
+	display(cur)
+	raw_input()
+	if check_all(cur):
+		return
+	if i+j == 16:
+		return cur
 
-		
+	j += 1
+	i += j / 9
+	j %= 9
+	for k in range(9):
+		temp = list(cur)
+		temp[i][j] = (init[i][j] + k) % 9 
+		result = gab_recurse(init, temp, i, j)
+		if result != None:
+			return result
+
+def get_solved_sudoku():
+	init = [[choice(range(9)) for i in range(9)] for j in range(9)]
+	cur = [['-' for i in range(9)] for j in range(9)]
+	return gab_recurse(init, cur, 0, -1)
+
+	result = [['-' for i in range(9)] for j in range(9)]
+	i,j = 0,0
+	while i < 9:
+		raw_input()
+		display(result)
+		result[i][j] = init[i][j]
+		print result
+		while check_all(result):
+			result[i][j] = result[i][j] + 1
+			if result[i][j] == 10:
+				result[i][j] = 1
+			if result[i][j] == init[i][j]:
+				result[i][j] = '-'
+				j -= 1
+				i += (j / 9)
+				j %= 9
+			else:
+				break
+		j += 1
+		i += (j / 9)
+		j %= 9
 	return result
+
+def get_sudoku(difficulty):
+	init = get_solved_sudoku()
 
 class MainWindow(wx.Frame):
 	def __init__(s, parent, title):
 		wx.Frame.__init__(s, parent, title=title, size=(400,-1))
-		initial_tiles = [[choice(range(1,10) + [' ']) for i in range(9)] for j in range(9)]
+		initial_tiles = [[choice(range(1,10) + ['-']) for i in range(9)] for j in range(9)]
 		s.CreateStatusBar()
 
 		# Draw everything
