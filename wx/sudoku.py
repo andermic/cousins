d89a656583f4e9283e3de2cb9b29426eda9fd23d
Mayor McCheese <andermic@Mayor-McCheeses-Mac-Pro.local>
2012-12-23 01:12:57 -0800

    dialog for clicking cells. Working on checking for solutions

--- wx/sudoku.py
+++ wx/sudoku.py
@@ -86,7 +86,7 @@ def solve(cur, i, j, rev=False):
 			return result 
 
 def get_sudoku(difficulty):
-	global EASY, MEDIUM, HARD, VERYHARD, INSANE
+	global DEBUG, EASY, MEDIUM, HARD, VERYHARD, INSANE
 	take_out_ranges = {EASY:(20,30), MEDIUM:(30,40), HARD:(40,45), VERYHARD:(45,55), INSANE:(55,60)}
 	got_sudoku = False
 	
@@ -107,6 +107,36 @@ def get_sudoku(difficulty):
 		if solve(seeds, 0, -1, False) == solve(seeds, 0, -1, True):
 			got_sudoku = True
 	return solution, seeds
+
+class ClickDialog(wx.Dialog):
+	def __init__(s, parent, id, title, p=(0,0)):
+		wx.Dialog.__init__(s, parent, id, title, size=(30,275), pos=p)
+		sizer = s.CreateTextSizer('')
+		s.choices = []
+		for i in range(1,10):
+			s.choices.append(wx.Button(s, -1, str(i), size=(27,25), name=str(i)))
+			sizer.Add(s.choices[-1])
+			s.choices[-1].Bind(wx.EVT_BUTTON, s.click_button)
+			s.choices[-1].Bind(wx.EVT_KEY_DOWN, s.keypress_button)
+		s.SetSizer(sizer)
+
+	def click_button(s, event):
+		s.EndModal(int(event.GetEventObject().GetName()))
+
+	def keypress_button(s, event):
+		if event.GetKeyCode() == 13:
+			s.click_button(event)
+
+
+	def keypress_button(s, event):
+		button = event.GetEventObject()
+		pos = int(button.GetName()) - 1
+		key = event.GetKeyCode()
+		if key == 13:
+			s.click_button(event)
+		elif key == 315 or key == 317:
+			s.choices[(pos + key - 316)%9].SetFocus()
+
 
 class MainWindow(wx.Frame):
 	# Draw things
@@ -158,6 +188,14 @@ class MainWindow(wx.Frame):
 		#s.grid.Fit(s)
 		s.Show()
 
+	def check_for_solution():
+		for i in range(9):
+			for j in range(9):
+				if s.solution[i][j] != s.cells[i][j].GetLabel()
+					return
+
+		print 'solution'
+
 	def on_paint(s, event):
 		dc = wx.PaintDC(s)
 		dc.Clear()
@@ -170,7 +208,7 @@ class MainWindow(wx.Frame):
 		dc.DrawLine(195, 0, 195, 227)
 
 	def draw_sudoku(s, difficulty):
-		solution, seeds = get_sudoku(difficulty)
+		s.solution, seeds = get_sudoku(difficulty)
 		for i in range(9):
 			for j in range(9):
 				cell = s.cells[i][j]
@@ -181,28 +219,34 @@ class MainWindow(wx.Frame):
 				else:
 					s.seeds[i][j] = True
 					cell.SetFont(s.fbold)
-		display(seeds)
 
 	def keypress_cell(s, event):
 		cell = event.GetEventObject()
 		pos = [int(i) for i in list(cell.GetName())]
 		key = event.GetKeyCode()
-		if key >= 49 and key <= 57 and (not s.seeds[pos[0]][pos[1]]):
+		if key == 13:
+			s.click_cell(event)
+		elif key >= 49 and key <= 57 and (not s.seeds[pos[0]][pos[1]]):
 			cell.SetLabel(str(key - 48))
+			s.check_for_solution()
 		elif key >= 314 and key <= 317:
 			dir = (0 if key%2==0 else key-316, 0 if key%2==1 else key-315)
 			s.cells[(pos[0]+dir[0])%9][(pos[1]+dir[1])%9].SetFocus()
 
 	def click_cell(s, event):
 		cell = event.GetEventObject()
-		pos = [int(i) for i in list(cell.GetName())]
-		if not s.seeds[pos[0]][pos[1]]:
-			choose_no = wx.SingleChoiceDialog(s, 'message', 'caption', ['1','2','a','b'])#(s, '', 'Choose a number', wx.OK)
+		loc = [int(i) for i in cell.GetName()]
+		if not s.seeds[loc[0]][loc[1]]:
+			pos = cell.GetPosition()
+			choose_no = ClickDialog(s, -1, '', (pos[0], pos[1] + 50))
 			choose_no.ShowModal()
+			if choose_no.GetReturnCode() / 10 == 0:
+				cell.SetLabel(str(choose_no.GetReturnCode()))
+				s.check_for_solution()
 			choose_no.Destroy()
 
 	def click_easy(s, event):
-		s.draw_sudoku(EASY)
+		s.draw_sudoku(DEBUG)
 
 	def click_medium(s, event):
 		s.draw_sudoku(MEDIUM)
