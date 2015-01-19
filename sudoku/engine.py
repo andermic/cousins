980f5a3ac50bb3f4847c1d02e5f74c97f9f8c5eb
Mike Anderson <ptolemy777@gmail.com>
2015-01-19 07:26:55 -0800

    Kenken test cases

--- sudoku/engine.py
+++ sudoku/engine.py
@@ -42,14 +42,12 @@ down = lambda x,n: (x[0]+1,x[1]) if x[0]!=n else None
 left = lambda x,n: (x[0],x[1]-1) if x[1]!=0 else None
 right = lambda x,n: (x[0],x[1]+1) if x[1]!=n else None
 
-# Generating a VHARD sudoku w/ seed=4 takes 11.2 seconds
 def check_for_duplicates(test_list):
     no_empty = [i for i in test_list if i != None]
     if len(set(no_empty)) == len(no_empty):
         return False
     return True
 
-# Generating a VHARD sudoku w/ seed=4 takes 8.7 seconds (w/o subsquare)
 def check_for_duplicates2(test_list, elem):
     have_seen = False
     for t in test_list:
@@ -58,8 +56,6 @@ def check_for_duplicates2(test_list, elem):
                 return True
             have_seen = True
     return False
-
-# TODO: Try storing the current state of each row,col,subsquare relative to individual digits
 
 def encrypt_decrypt(str):
     return ''.join([chr((ord(i) + 128)%256) for i in str])[::-1]
@@ -228,10 +224,7 @@ class Sudoku(Puzzle):
 
 
 class Kenken(Puzzle):
-    def semigraphics1(s, board):
-        cells, section_list, section_matrix, section_labels = board
-        n = len(cells)
-        
+    def semigraphics1(s, cells, n, section_matrix):
         print UNI_DOUB_ELB_SE + UNI_DOUB_T_DOWN.join(n*[UNI_DOUB_HORI]) + UNI_DOUB_ELB_SW
         for i in range(n):
             vert_separators = [' ' if ((section_matrix[i][j]) == section_matrix[i][j+1]) else UNI_DOUB_VERT for j in range(n-1)]
@@ -242,10 +235,7 @@ class Kenken(Puzzle):
         print UNI_DOUB_ELB_NE + UNI_DOUB_T_UP.join(n*[UNI_DOUB_HORI]) + UNI_DOUB_ELB_NW
         print
     
-    def semigraphics2(s, board):
-        cells, section_list, section_matrix, section_labels = board
-        n = len(cells)
-
+    def semigraphics2(s, cells, n, section_matrix):
         print UNI_DOUB_ELB_SE + ' '.join(n*[4*UNI_DOUB_HORI]) + UNI_DOUB_ELB_SW
         for i in range(n):
             vert_separators = [' ' if ((section_matrix[i][j]) == section_matrix[i][j+1]) else UNI_DOUB_VERT for j in range(n-1)]
@@ -258,37 +248,184 @@ class Kenken(Puzzle):
         print UNI_DOUB_ELB_NE + ' '.join(n*[4*UNI_DOUB_HORI]) + UNI_DOUB_ELB_NW
         print
         
-    def display(s, board):
-        SEMIGRAPHICS = s.semigraphics2
-        SEMIGRAPHICS(board)
-        
+    def display(s, board, sg_only=False):
         cells, section_list, section_matrix, section_labels = board
         n = len(cells)
         
+        SEMIGRAPHICS = s.semigraphics2
+        SEMIGRAPHICS(cells, n, section_matrix)
+        
+        if sg_only:
+            return
+
         for i in range(len(section_list)):
             print 'Section %d:\t%s\t%s' % (i, section_labels[i], ' '.join([str(j) for j in section_list[i]]))
+        
+    def get_solve_test_cases(s):
+        s.test_cases = []
 
+        # Test case 1
+        
+        n = 3
+
+        section_list = []
+        section_list.append([(0,0), (0,1)])
+        section_list.append([(0,2)])
+        section_list.append([(1,0), (2,0)])
+        section_list.append([(1,1), (1,2)])
+        section_list.append([(2,1), (2,2)])
+        
+        # TODO: Modularize this block
+        section_matrix = [n*[None] for i in range(n)]
+        for i in range(len(section_list)):
+            for j in range(len(section_list[i])):
+                r,c = section_list[i][j][0], section_list[i][j][1]
+                section_matrix[r][c] = i
+
+        section_labels = ['-1', '1', '-1', '/3', '*6']
+
+        solution = []
+        solution.append([3,2,1])
+        solution.append([2,1,3])
+        solution.append([1,3,2])
+        
+        s.test_cases.append({})
+        s.test_cases[-1]['n'] = n
+        s.test_cases[-1]['section_list'] = section_list
+        s.test_cases[-1]['section_matrix'] = section_matrix
+        s.test_cases[-1]['section_labels'] = section_labels
+        s.test_cases[-1]['solution'] = solution
+        
+        
+        # Test case 2
+        
+        n = 6
+        
+        section_list = []
+        section_list.append([(0,0), (1,0)])
+        section_list.append([(0,1), (0,2)])
+        section_list.append([(0,3), (0,4)])
+        section_list.append([(0,5), (1,5)])
+        section_list.append([(1,1), (2,1)])
+        section_list.append([(1,2), (2,2)])
+        section_list.append([(1,3), (2,3)])
+        section_list.append([(1,4), (2,4)])
+        section_list.append([(2,0), (3,0), (3,1)])
+        section_list.append([(2,5), (3,4), (3,5)])
+        section_list.append([(3,2), (3,3)])
+        section_list.append([(4,0), (4,1), (5,0)])
+        section_list.append([(4,2), (4,3)])
+        section_list.append([(4,4), (4,5), (5,5)])
+        section_list.append([(5,1), (5,2)])
+        section_list.append([(5,3), (5,4)])
+        
+        section_matrix = [n*[None] for i in range(n)]
+        for i in range(len(section_list)):
+            for j in range(len(section_list[i])):
+                r,c = section_list[i][j][0], section_list[i][j][1]
+                section_matrix[r][c] = i
+        
+        section_labels = ['-2','-1','*10','/3','+11','+7','-5','/3','+9','*120','+3','+7','-1','+13','/3','-1']
+        
+        solution = []
+        solution.append([6,3,4,5,2,1])
+        solution.append([4,5,2,6,1,3])
+        solution.append([2,6,5,1,3,4])
+        solution.append([3,4,1,2,5,6])
+        solution.append([5,1,3,4,6,2])
+        solution.append([1,2,6,3,4,5])
+        
+        s.test_cases.append({})
+        s.test_cases[-1]['n'] = n
+        s.test_cases[-1]['section_list'] = section_list
+        s.test_cases[-1]['section_matrix'] = section_matrix
+        s.test_cases[-1]['section_labels'] = section_labels
+        s.test_cases[-1]['solution'] = solution
+        
+    def run_solve_test_cases(s):
+        s.get_solve_test_cases()
+        
+        for i in range(len(s.test_cases)):
+            cur_list = s.test_cases[i]['section_list']
+            cur_matrix = s.test_cases[i]['section_matrix']
+            cur_labels = s.test_cases[i]['section_labels']
+            cur_solution = s.test_cases[i]['solution']
+            cur_n = len(cur_matrix)
+            
+            print 'TEST CASE %d\n' % (i+1)
+            
+            init = cur_n*[cur_n*[-1]]
+            cur = [cur_n*[None] for i in range(cur_n)]
+            found_solution = s.solve(init, cur, cur_n, cur_list, cur_matrix, cur_labels, 0, -1)
+            print 'Solution found:'
+            s.display((found_solution, cur_list, cur_matrix, cur_labels), True)
+            print
+            
+            print 'Actual solution:'
+            s.display((cur_solution, cur_list, cur_matrix, cur_labels), True)
+            print
+            
+            if found_solution == cur_solution:
+                print 'Puzzle was solved correctly!'
+            else:
+                print 'Puzzle was NOT solved correctly'
+            print
+            print
+    
+    # TODO: Strongly consider solving section-by-section rather than row-by-row.
+    # Would likely make the average search space orders of magnitude smaller.
     def solve(s, init, cur, n, section_list, section_matrix, section_labels, i, j):
+        #s.display((cur, section_list, section_matrix, section_labels), True)
+        print cur
+        #raw_input()
+        print
+        
+        # TODO: Consider optimizing using check_for_duplicates2
         if check_for_duplicates(cur[i]) or \
          check_for_duplicates([cur[k][j] for k in range(n)]):
             return
         if i == n-1 and j == n-1:
             return cur
         
-        # TODO: If latest cell belongs to a section then check section constraint
-        # Can backtrack on * and + as soon as current prod/sum goes over expected total
-        
+        if not(section_list == [] or j == -1):
+            section_num = section_matrix[i][j]
+            cur_label = section_labels[section_num]
+            
+            if cur_label[0] in ['+','-','*','/']:
+                operator, section_result = cur_label[0], int(cur_label[1:])
+            else:
+                operator, section_result = '', int(cur_label)
+            
+            cur_section_list = section_list[section_num]
+            vals = [cur[sl[0]][sl[1]] for sl in section_list[section_num]]
+            vals = [str(v) for v in vals if v != None]
+            
+            # TODO: Consider optimizing for speed by memoizing section sub-totals
+            if operator in ['+','*']:
+                total = eval(operator.join(vals))
+                if total > section_result:
+                    return
+                if len(vals) == len(cur_section_list) and total != section_result:
+                    return
+            elif operator in ['-','/']:
+                if len(vals) == 2:
+                    vals.sort(key=lambda x: str(x))
+                    if eval(vals[1] + operator + vals[0]) != section_result:
+                        return
+            elif operator == '':
+                if int(vals[0]) != section_result:
+                    return
         
         j += 1
         i += j / n
         j %= n
         
         for k in range(1, n + 1):
-            cur = [l[:] for l in cur]
             cur[i][j] = (init[i][j] + k) % n + 1
             result = s.solve(init, cur, n, section_list, section_matrix, section_labels, i, j)
             if result != None:
                 return result
+            cur[i][j] = None
     
     def get_sections(s, cur_section_list, cur_section_matrix, n, section_sizes):
         unassigned = [(i,j) for i in range(n) for j in range(n) if cur_section_matrix[i][j] == None]
@@ -318,8 +455,8 @@ class Kenken(Puzzle):
     
     def get_solved_board(s, n):
         init = [[choice(range(n)) for i in range(n)] for j in range(n)]
-        cur = [[None] * n for i in range(n)]
-        init_section_matrix = [[None] * n for i in range(n)]
+        cur = n*[n*[None]]
+        init_section_matrix = n*[[None]]
         cells = s.solve(init, cur, n, [], init_section_matrix, [], 0, -1)
         
         # TODO: Put the logic in this block into get_sections (?),
@@ -352,7 +489,7 @@ class Kenken(Puzzle):
             
         return cells, section_list, section_matrix, section_labels
     
-    #TODO: Implement. Ensure puzzle has a unique solution.
+    #TODO: Finish implementing. Ensure puzzle has a unique solution.
     def get_puzzle(s, difficulty):
         got_kenken = False
         
@@ -361,18 +498,7 @@ class Kenken(Puzzle):
             count += 1
             print count
             
-            solution = s.get_solved_board()
-            seeds = [i[:] for i in solution]
-            is_fixed = [[True] * 9 for i in range(9)]
-            
-            take_out_no = choice(range(*take_out_ranges[difficulty]))
-            i = 0
-            while i < take_out_no:
-                r,c = choice(range(9)), choice(range(9))
-                if seeds[r][c] != None:
-                    seeds[r][c] = None
-                    is_fixed[r][c] = False
-                    i += 1
+            cells, section_list, section_matrix, section_labels = s.get_solved_board(difficulty)            
             
             got_kenken = (solution == s.solve(solution, seeds, is_fixed, 0, -1))
         
